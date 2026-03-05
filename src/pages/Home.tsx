9a8644dbb6b1a3021ebeb47b4dbd41c7b93bbe1e
Tantalus-Geomatics <dennis@tantalusgeomatics.com>
2026-03-04 18:15:38 -0800

    Updated home images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -173,7 +173,8 @@ export default function Home() {
                       <img 
                         src={useCase.image} 
                         alt={useCase.title}
-                        className="w-full h-40 sm:h-48 object-cover rounded-xl mb-5 grayscale opacity-100"
+                        className="w-full h-40 sm:h-48 object-cover rounded-xl mb-5 grayscale"
+                        //className="object-cover h-full w-full border border-white/10 grayscale hover:grayscale-0 transition-all duration-500"
                       />
                       <p className="text-white/70 font-light leading-relaxed text-sm sm:text-base">
                         {useCase.content}
